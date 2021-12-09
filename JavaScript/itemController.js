0579b2e752596ef79bf8bcc3e6ce2bb092a350ba
Anna <programming.aap@gmail.com>
2021-12-09 06:28:18 -1000

    add

--- JavaScript/itemController.js
+++ JavaScript/itemController.js
@@ -17,7 +17,7 @@ class outfitController {
   }
 
   setLocalStorage() {
-    let storeOutfit = JSON.stringify(this.outfit);
+    let storeOutfit = JSON.stringify(this.outfitArr);
     localStorage.setItem('outfit', storeOutfit);
     let currentId = JSON.stringify(this.currentId);
     localStorage.setItem('currentId', currentId);
@@ -25,11 +25,15 @@ class outfitController {
 
   loadLocalStorage() {
     if(localStorage.getItem('outfit')) {
-      
+      let outfitArrayJson = localStorage.getItem('outfit');
+      console.log(outfitArrayJson);
+      console.log(JSON.parse(outfitArrayJson));
+      this.outfit = JSON.parse(outfitArrayJson);
     }
     
     if(localStorage.getItem('currentId')) {
-
+      let currentIdString = localStorage.getItem('currentId');
+      this.currentId = Number(currentIdString);
     }
   };
 
@@ -38,6 +42,6 @@ class outfitController {
 
 
 
-let newOutfit = new outfitController();
-newOutfit.addOutfit('someUrl',15, 'dress red');
-console.log(newOutfit)
+// let newOutfit = new outfitController();
+// newOutfit.addOutfit('someUrl',15, 'dress red');
+// console.log(newOutfit)
